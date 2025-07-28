67e277f0c8bb6e5f3d30e06d53bcb93821c9fbc5
SP_ctrl <157314519+Soundpauli@users.noreply.github.com>
2025-07-28 16:22:06 +0200

    Updated LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -104,11 +104,7 @@ const LoginPage = () => {
 
           <div className="mt-6">
             <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-300" />
-              </div>
-              
-            </div>
+           
 
          
           </div>
